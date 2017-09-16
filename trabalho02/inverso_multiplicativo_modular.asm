ed05e005597cc6dc940e33dbe047b5c754ffe491
Cecilia Dib <ceciliabessa@hotmail.com>
2017-09-16 15:42:49 -0300

    Evolving code

--- trabalho02/inverso_multiplicativo_modular.asm
+++ trabalho02/inverso_multiplicativo_modular.asm
@@ -1,26 +1,59 @@
 .data
 primo: 		.word 		0			 		# primeiro numero a ser lido
 numero: 	.word 		0					# segundo numero a ser lido
-string1: 	.asciiz 	"O†inverso†multiplicativo†eh "		# string para mostrar o resultado da operaÁ„o
-string2: 	.asciiz		"O†modulo†nao†eh†primo"			# string para mostrar que n„o È primo
+string1: 	.asciiz 	"O inverso multiplicativo eh "		# string para mostrar o resultado da opera√ß√£o
+string2: 	.asciiz		"O modulo nao eh primo"			# string para mostrar que n√£o √© primo
 
 .text
 main:
-	la $a0, primo		# armazena o endereÁo de memÛria de number1 em $a0
-	la $a1, numero		# armazena o endereÁo de memÛria de number2 em $a1
-	jal le_inteiros		# pula para o local de memÛria "le_inteiros" esperando o seu retorno
-	jal eh_primo		# pula para o local de memÛria "eh_primo" esperando o seu retorno
-	
+	la $a0, primo			# armazena o endere√ßo de memoria de number1 em $a0
+	la $a1, numero			# armazena o endere√ßo de memoria de number2 em $a1
+	jal le_inteiros			# pula para o local de memoria "le_inteiros" esperando o seu retorno
+	blt $a0, 2, imprime_erro 	# se o numero1 for menor que 2, imprime o erro
+	jal eh_par			# se o n√∫mero for par, imprime o erro
+ 	beq $a0, 2, calc_inverso 	# se o numero1 for igual a 2, calcula o inverso pois ele √© primo
+	jal eh_primo			# pula para o local de memoria "eh_primo" esperando o seu retorno
+ 	
 le_inteiros:
-	li $v0, 5		# carrega 5 em $v0 para chamar a funÁ„o de read Integer
-	syscall			# executa a chamada da funÁ„o
-	la $t0, 0($v0)		# armazena o valor lido em $t0
+	li $v0, 5			# carrega 5 em $v0 para chamar a fun√ß√£o de read Integer
+	syscall				# executa a chamada da fun√ß√£o
+	la $t0, 0($v0)			# armazena o valor lido em $t0
 	li $v0, 5
 	syscall
-	la $t1, ($v0)		# armazena o valor lido em $t1
-	sw $t0, ($a0)		# armazena o valor de $t0 no local de memÛria de $a0 (vari·vel 'primo')
-	sw $t1, ($a1)		# armazena o valor de $t1 no local de memÛria de $a1 (vari·vel 'numero')
-	jr $ra			# retorna para a funÁ„o que o chamou
+	la $t1, ($v0)			# armazena o valor lido em $t1
+	sw $t0, ($a0)			# armazena o valor de $t0 no local de memoria de $a0 (variavel 'primo')
+	sw $t1, ($a1)			# armazena o valor de $t1 no local de memoria de $a1 (variavel 'numero')
+	jr $ra				# retorna para a fun√ß√£o que o chamou
 	
+eh_par: 
+	divu $t1, 2			# divide o n√∫mero por 2 para verificar se √© par
+	mflo $v1			# modulo da divis√£o de t1/2 em $v1
+	beqz $v1, imprime_erro		# t1 √© par
+	jr $ra				# retorna para a fun√ß√£o que o chamou
+
 eh_primo:
+	subi $t2, $t1, 1		# t2 = t1 - 1
+	div $t1, $t2			# t1/t2
+  	mfhi $t3 			# resultado de t1/t2 em $t3
+  	mflo $v0 			# quociente da divis√£o de t1/t2 em $v0
+  	beqz $v0, imprime_erro
+  	ori $t4, $0, 3			# come√ßa a contagem a partir de 3, $t4 √© o contador
+  	j loop
+ 
+loop:
+	divu $a0, $t4			# numero/contador
+	mflo $v0			# quociente da divis√£o a0/t4
+	
+	addi $t4, $t4, 2		# contador incrementa de 2 em 2
+		
+  	
+imprime_erro:
+	la $a2, string2			# armazena o endere√ßo de memoria de string2 em $a0
+	li $v0, 4			# fun√ß√£o para imprimir string
+	syscall
+
+imprime_saida:
+	li $a0, 10			# fun√ß√£o para terminar o programa
+	syscall
+	
 	
